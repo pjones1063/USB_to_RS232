9cf081254db3544e1c04ec5616a000ad3398a3f0
Paul Jones <pjones1063@gmail.com>
2019-07-26 16:33:15 -0400

    src_1907026_1

--- Atari_16bit/LDATE.C
+++ Atari_16bit/LDATE.C
@@ -18,6 +18,14 @@
 #define MIDI 3	/* Atari MIDI port */
 #define KBD 4	/* Atari Keyboard port */
 
+const int dow[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+int       dom[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+const char * mTxt[] =
+     { 
+	"January", "February", "March", "April", "May", "June", 
+	"July", "August", "September", "October","November", "December"
+     };
+
 struct dateinfo {
   short ti_year;
   char  ti_day;
@@ -31,20 +39,62 @@ struct timeinfo {
   unsigned char   ti_sec;
 };
 
+/* Find our DOW  */
+int dayofweek(int d, int m, int y) 
+{ 
+    if(y%400 == 0) dom[2] = 29;
+    y -= m < 3; 
+    return ( y + y/4 - y/100 + y/400 + dow[m-1] + d) % 7; 
+} 
 
-int printDt() {
 
+int printDt() 
+{
     struct dateinfo dinf;
     struct timeinfo tinf;
     getdate(&dinf);
     gettime(&tinf);
-    printf("\n\n * current time set to -> %02i/%02i/%4i   %02i:%02i:%02i\n  ", 
+    printf("\n\n* SysDate -> %02i/%02i/%4i %02i:%02i:%02i\n  ", 
          dinf.ti_month, dinf.ti_day, dinf.ti_year,
          tinf.ti_hour,  tinf.ti_min, tinf.ti_sec );
-
    return 1;
 }
 
+
+int printCl() 
+{
+  int lg, dg, dinm, d, x;  
+  struct dateinfo dinf;
+  struct timeinfo tinf;  
+
+  getdate(&dinf);
+  gettime(&tinf);
+  dinm = dom[dinf.ti_month - 1];  
+  lg = dayofweek(1, dinf.ti_month, dinf.ti_year);
+  
+  printf("\n %s %02i %02i    %02i:%02i:%02i  \n",
+        mTxt[dinf.ti_month - 1], dinf.ti_day, dinf.ti_year, 
+        tinf.ti_hour,  tinf.ti_min, tinf.ti_sec );
+  printf("\n Su  Mn  Tu  We  Th  Fr  Sa \n");
+  
+  for (d = 1; d <= 42;)
+  {
+	printf("\n");
+	for(x=1; x<=7; x++)
+    {
+     dg = d - lg;
+     if(dg == dinf.ti_day)
+       printf(" \033p%02d\033q ", dg)
+     else if(d > lg && d <= dinm + lg) 
+       printf(" %02d ", dg);
+     else 
+       printf("    ");
+
+    d++;     
+	}		
+   }
+  return 1;
+}
 
 int setDt(char *i)
 {
@@ -132,8 +182,8 @@ int main(void)
        setTm(&tm);
    }
 
-    printDt();
-    sleep(1);
+    printCl();
+    sleep(2);
     return 0;
 }
 
